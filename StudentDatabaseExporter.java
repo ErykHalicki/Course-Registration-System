f3f6b827130815533b78a5a7577e4e75d23670dd
AakashSinghCS <ads2004mayads@gmail.com>
2025-02-04 13:39:44 -0800

    Implemented student list export to text file.

---
+++ StudentDatabaseExporter.java
@@ -0,0 +1,54 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDatabaseExporter {
+    
+    public static List<Student> fetchStudentsFromDB() {
+        List<Student> students = new ArrayList<>();
+        String url = "jdbc:mysql://"; // JDBC connection once database is set up. 
+        String user = ""; // userid of database once set up.
+        String password = ""; // password of userid once database set up.
+        
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM students")) { // Selects everything from STUDENTS database.
+
+            while (rs.next()) {
+                students.add(new Student(/* The getters will change based on Student attributes in the database.
+                
+                                         rs.getInt("id"),       
+                                         rs.getString("name"), 
+                                         rs.getInt("age"), 
+                                         rs.getString("grade")));
+                                         */
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+
+    public static void writeStudentsToFile(List<Student> students, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (Student student : students) {
+                writer.write(student.toString());
+                writer.newLine();
+            }
+            System.out.println("Student data successfully written to " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = fetchStudentsFromDB();
+        writeStudentsToFile(students, "TextFileName.txt"); // text file to export to goes here.
+    }
+}
