91df80674b9d8af6d39c882435861bed16295184
ErykHalicki <erykhalicki0@gmail.com>
2025-03-26 09:38:08 -0700

    fixed admin course uses bugs

--- vaadin_project/src/main/java/recordrangers/services/DatabaseConnection.java
+++ vaadin_project/src/main/java/recordrangers/services/DatabaseConnection.java
@@ -13,20 +13,11 @@ public class DatabaseConnection {
             String url;
             String user = "root";
             String password = "";
-
-            if (System.getProperty("useH2") != null) {
-                // Use H2 in-memory database for testing
-                url = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
-                user = "sa";  // Default H2 user
-                password = ""; // No password needed for H2
-            } else {
-                // Use MySQL for production
-            	//jdbc:mysql://database:3306/UniversityDB
-                url = "jdbc:mysql://localhost:3306/UniversityDB";
-                user = "root";
-                password = "";
-            }
-
+            // Use MySQL for production
+            //jdbc:mysql://database:3306/UniversityDB
+            url = "jdbc:mysql://localhost:3306/UniversityDB";
+            user = "root";
+            password = "";
             this.connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
